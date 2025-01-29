a15688c62edc76f9145edb994aa9267e330934a8
Ilektra Christidi <ilektra.christidi@ucl.ac.uk>
2025-01-29 15:49:45 +0000

    Read test parameters as well from the file, remove from command line.

--- tests/run_regression_test.py
+++ tests/run_regression_test.py
@@ -24,24 +24,27 @@ def read_expected_values(test_name, grid="8.8.8.8", mpi="1.1.1.1"):
 
 def read_expected_values_line(test_name, line_number=1):
     """
-    Read expected values from file.
+    Read test parameters and expected values from file.
 
     The file contains one or more entries of the following format:
     <line_number> <grid> <mpi> <plaquette> <checksum_rng> <checksum_lat>
     Eg.
     1 8.8.8.8 1.1.1.1 0.0256253844 922c392f d1e4cc1c
 
-    This function will return the values only for the requested line_number.
+    This function will return the test parameters and expected values 
+    only for the requested line_number.
     """
 
+    test_parameters = {}
     with open(f"{test_name}_expected.txt") as file:
         for line in file:
             line_split = line.split()
             if line_split[0] == line_number:
                 print(f"Reading reference values for grid={line_split[1]}, mpi={line_split[2]}")
-                return float(line_split[3]), line_split[4], line_split[5]
+                test_parameters = {'grid': line_split[1], 'mpi': line_split[2]}
+                return test_parameters, float(line_split[3]), line_split[4], line_split[5]
 
-    return None, None, None
+    return test_parameters, None, None, None
 
 
 def read_output():
@@ -67,21 +70,21 @@ def read_output():
     return plaquette, checksum_rng, checksum_lat
 
 
-def test_outputs(test_name, grid, mpi, expected_line, cleanup_files):
+def test_outputs(test_name, expected_line, cleanup_files):
     import subprocess
     import os
 
-    expected_plaquette, expected_checksum_rng, expected_checksum_lat = read_expected_values_line(test_name, expected_line)
-    if (expected_plaquette is None) or (expected_checksum_rng is None) or (expected_checksum_lat is None):
+    test_parameters, expected_plaquette, expected_checksum_rng, expected_checksum_lat = read_expected_values_line(test_name, expected_line)
+    if (not test_parameters) or (expected_plaquette is None) or (expected_checksum_rng is None) or (expected_checksum_lat is None):
         pytest.fail(f"No appropriate reference values found, check {test_name}_expected.txt")
 
+    print(f"Running {test_name} for test parameters: ", test_parameters)
     #result = subprocess.run([f"./{test_name} --grid {grid} --mpi {mpi} --Thermalizations 0 --Trajectories 1 --threads 1 > output.txt"], shell=True, encoding="text")
-    #result = subprocess.run([f"./{test_name} --grid {grid} --mpi {mpi} --Thermalizations 0 --Trajectories 1 > output.txt"], shell=True, encoding="text")
+    result = subprocess.run([f"./{test_name} --grid {test_parameters['grid']} --mpi {test_parameters['mpi']} --Thermalizations 0 --Trajectories 1 > output.txt"], shell=True, encoding="text")
     plaquette, checksum_rng, checksum_lat = read_output()
     if (checksum_rng is None) or (checksum_lat is None) or (plaquette is None):
         pytest.fail("Error reading values from output file. Make sure you compile the test with CPparams.saveInterval=1 in order to produce the required output.")
         
-    print(f"Running {test_name}")
     # This manual check of each condition doesn't have to happen for pytest-check
     # version 1.2.0 and later. We can use any_failures() instead.
     failed = False
